fe8340327603cd9ad98cb2d4360ccaf5386c49bf
KorDP <nukedmitry@gmail.com>
2023-01-18 20:46:00 +0300

    initial commit

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"Большее число: {a}");
+}
+else if (a < b)
+{
+    Console.WriteLine($"Большее число: {b}");
+} else 
+{
+    Console.WriteLine("Числа одинаковы");
+}
