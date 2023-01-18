ed8cbe2665a08d670a758d167c82155aa306d658
KorDP <nukedmitry@gmail.com>
2023-01-18 21:02:35 +0300

    ДЗ номер 1 сделал

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 > n2 && n1 > n3)
+{
+    Console.WriteLine($"Максимальное число: {n1}");
+}
+else if (n2 > n1 && n2 > n3)
+{
+    Console.WriteLine($"Максимальное число: {n2}");
+} 
+else if (n3 > n1 && n3 > n2)
+{
+    Console.WriteLine($"Максимальное число: {n3}");
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
---
+++ task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine($"{num} число четное");
+}
+else 
+{
+    Console.WriteLine($"{num} число нечетное");
+}
